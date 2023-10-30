e68a76ede547196122a974892ea521383e810a0d
ian <ianstreator+github@gmail.com>
2023-10-30 17:03:58 -0500

    lint fix

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,21 +1,20 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { useUserStore } from "../state";
-import authenticate from "../utils/authenticate";
+// import authenticate from "../utils/authenticate";
 
 function NotFound() {
   const {
     user: { _id },
   } = useUserStore.getState();
 
-  const validUser = _id && authenticate;
+  const validUser = _id;
   const redirect = validUser ? `/user/${_id}` : "/login";
   return (
-    <div className="p-10 flex flex-col items-center justify-center">
-      <h1>404 Error!</h1>
-      <h1>You look lost, click the button to get back to where you belong.</h1>
+    <div className="p-10 flex flex-col items-center text-center justify-center">
+      <h1 className="font-bold text-2xl pb-4">Error 404 page not found</h1>
+      <p className="text-7xl">🕵️👮‍♂️</p>
       <Link to={redirect} className="btn-style">
-        back
+        police line do not cross 
       </Link>
     </div>
   );
